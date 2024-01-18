7f03c4af08c1ad3af275078362bc564395133926
Hoang Gia <hoanggiainfomation@gmail.com>
2024-01-18 12:12:44 +0700

    adasd

--- src/theme.js
+++ src/theme.js
@@ -6,18 +6,24 @@ import { cyan, deepOrange, orange, teal } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '88px',
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange,
       },
+      spacing: (factor) => `${0.25 * factor}rem`,
     },
     dark: {
       palette: {
         primary: cyan,
         secondary: orange,
       },
+      spacing: (factor) => `${0.25 * factor}rem`,
     },
   },
   // ...other properties
